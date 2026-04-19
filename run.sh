76597d6676f49f23cc39d193b0d1f75848e5eff0
Aspa <asspatic@gmail.com>
2026-04-19 14:13:17 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,28 +1,23 @@
 #!/bin/bash
 
-FILE="fedora-toolbox.html"
 
 echo "[*] Açılıyor: $FILE"
 
 # Termux (Android)
 if command -v termux-open > /dev/null 2>&1; then
-    termux-open "$FILE"
+    termux-open "termux-toolbox.html"
 
 # Linux
 elif command -v xdg-open > /dev/null 2>&1; then
-    xdg-open "$FILE" >/dev/null 2>&1 &
-
-# macOS
-elif command -v open > /dev/null 2>&1; then
-    open "$FILE"
+    xdg-open "fedora-toolbox.html" >/dev/null 2>&1 &
 
 # Windows (Git Bash / WSL fallback)
 elif command -v start > /dev/null 2>&1; then
-    start "$FILE"
+    start "win-toolbox.html"
 
 else
     echo "[!] Tarayıcı açılamadı."
-    echo "[!] Dosyayı manuel aç: $FILE"
+    echo "[!] Dosyayı manuel aç"
     exit 1
 fi
 
